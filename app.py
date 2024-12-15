47334ced4f3d9dd10f43abc382e35a01be80dd91
Bohdan Bodakva <bodakvabohdan44@gmail.com>
2024-12-15 15:18:49 +0200

    removed test route

--- app.py
+++ app.py
@@ -16,43 +16,6 @@ socketio = SocketIO(app, cors_allowed_origins=env_vars['SOCKETIO_CORS_ALLOWED_OR
 
 
 
-@app.route("/")
-def hello_world():
-    with app.app_context():
-        db.drop_all()
-        db.create_all()
-
-    user1 = User(
-        username='user1',
-        name='User 1',
-        password='xxx'
-    )
-
-    user2 = User(
-        username='user2',
-        name='User 2',
-        password='xxx'
-    )
-
-    chat = Chat(
-        name='chat 1',
-        users=[user1, user2]
-    )
-
-    message1 = Message(
-        text="some msg 1",
-        chat_id=1
-    )
-
-    db.session.add_all([user1, user2])
-    db.session.add_all([chat])
-    db.session.add_all([message1])
-    db.session.commit()
-
-    all_users = Chat.query.all()
-
-    return jsonify([user.serialize() for user in all_users])
-
 
 if __name__ == '__main__':
 
