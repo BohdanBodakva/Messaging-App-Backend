80df7f8c9770a92c2e5a12eb627839de3aa20b52
Bohdan Bodakva <bodakvabohdan44@gmail.com>
2024-12-15 15:43:31 +0200

    some serialization fixes; returned test route

--- models/db_models.py
+++ models/db_models.py
@@ -4,28 +4,40 @@ from datetime import datetime
 
 db = SQLAlchemy()
 
+user_chat = db.Table('user_chat',
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
+    db.Column('chat_id', db.Integer, db.ForeignKey('chats.id'), primary_key=True)
+)
+
 class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(100), unique=True, nullable=False)
     profile_photo_link = db.Column(db.Text, nullable=False, default="")
     name = db.Column(db.String(100), nullable=False)
     surname = db.Column(db.String(100))
+    last_seen = db.Column(db.DateTime, nullable=False, default=datetime.now())
     password = db.Column(db.String(100), nullable=False)
+    chats = db.relationship('Chat', secondary=user_chat, back_populates=__tablename__, lazy='joined')
 
     def __repr__(self):
         return f"<User {self.id}: '{self.username}'>"
     
-    def serialize(self):
-        return {
+    def serialize(self, include_chats=True):
+        user = {
             "id": self.id,
             "username": self.username,
             "name": self.name,
             "surname": self.surname,
-            "password": self.password
+            "last_seen": self.last_seen,
+            "profile_photo_link": self.profile_photo_link
             }
+        if include_chats:
+            user["chats"] = [chat.serialize() for chat in self.chats]
 
+        return user
+    
 
 class Message(db.Model):
     __tablename__ = 'messages'
@@ -50,21 +62,16 @@ class Message(db.Model):
             "chat": self.chat_id
             }
 
-
-user_chat = db.Table('user_chat',
-    db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
-    db.Column('chat_id', db.Integer, db.ForeignKey('chats.id'), primary_key=True)
-)
-
 class Chat(db.Model):
     __tablename__ = 'chats'
 
     id = db.Column(db.Integer, primary_key=True)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
     name = db.Column(db.String(100), default="unnamed chat")
-    users = db.relationship('User', secondary=user_chat, backref=db.backref(__tablename__, lazy='joined'))
+    chat_photo_link = db.Column(db.Text, nullable=False, default="")
+    users = db.relationship('User', secondary=user_chat, back_populates=__tablename__, lazy='joined')
     messages = db.relationship('Message', backref=__tablename__, lazy='joined')
-    
+
     def __repr__(self):
         return f"<Chat {self.id}>"
     
@@ -72,7 +79,8 @@ class Chat(db.Model):
         return {
             "id": self.id,
             "name": self.name,
+            "chat_photo_link": self.chat_photo_link,
             "created_at": self.created_at,
-            "users": [user.serialize() for user in self.users],
+            "users": [user.serialize(include_chats=False) for user in self.users],
             "messages": [message.serialize() for message in self.messages]
             }
